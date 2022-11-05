c51b111b6e962433d2b05745d209862d6ce9e590
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-11-05 20:42:02 +0100

    added computeInput function

--- functions.js
+++ functions.js
@@ -1,8 +1,7 @@
 // TODO :
 // When undizzy is full damage scaling start at 0.55
 
-function comboDamageCalculation(arrayCombo, ratio)
-{
+function comboDamageCalculation(arrayCombo, ratio) {
     res = 0;
     i = 0;
     arrayCombo.forEach(function(item){
@@ -13,8 +12,7 @@ function comboDamageCalculation(arrayCombo, ratio)
     return res;
 }
 
-function moveDamageCalculation(move, position, ratio)
-{
+function moveDamageCalculation(move, position, ratio) {
     baseDamage = move.damage;
     scaling = scalingCalculation(move, position, ratio);
     damage = baseDamage * scaling;
@@ -25,8 +23,7 @@ function moveDamageCalculation(move, position, ratio)
     return damage;
 }
 
-function scalingCalculation(move, position, ratio)
-{
+function scalingCalculation(move, position) {
     scalingTmp = theoricScaling(move, position);
     scaling = scalingTmp;
     baseDamage = move.damage;
@@ -39,13 +36,32 @@ function scalingCalculation(move, position, ratio)
     return scaling;
 }
 
-function theoricScaling(move, position)
-{
+function theoricScaling(move, position) {
     if (position < 3) {
         return 1;
     } 
 
     return scaling = theoricScaling(move, position-1)*(0.875);
+}
+
+function computeInput() {
+
+    let character = document.getElementById("character").value;
+    let ratio = document.getElementById("ratio").value;
+
+    let inputList = document.getElementById("input-list").value;
+    inputs = inputList.split(' ');
+
+    let combo = [];
+    inputs.forEach(element => combo.push(eval(character).moves[element]));
+
+    // console.log(combo);
+    // console.log(comboDamageCalculation(combo, 1));
+
+    document.getElementById('result').innerHTML = comboDamageCalculation(combo, ratio);
+
+    // jHP jHK sHP jHP jHK jLK jHP jHK cLK sHP jLP jHP jHK
+    
 }
 
 combo = [
